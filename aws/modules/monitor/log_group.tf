3d92327a1cbefb063be37b0747dead764203c716
Emmanuel Rodríguez <rorem.94.developer@gmail.com>
2023-07-11 18:55:59 -0600

    docs: 📚added documentation for monitoring module

--- aws/modules/monitor/log_group.tf
+++ aws/modules/monitor/log_group.tf
@@ -1,3 +1,13 @@
+/**
+ * ℹ️Description
+ * This resource is responsible for creating a CloudWatch log group to group
+ * all the log streams to monitor the server outputs.
+ *
+ * 📝Attributes:
+ * - name: Name of the group, following the name convention: <project>-<service>.
+ *         For example: dashboard-server.
+ * - retention_in_days: Number of days to keep the logs.
+ */
 resource "aws_cloudwatch_log_group" "server" {
   name              = local.monitor.server.log.group.name
   retention_in_days = 14
